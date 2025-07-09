fe76760b9b3046170d1eee01e8ea06b8bda7b125
Rod-Sot <r.sotomayor.2021@alumnos.urjc.es>
2025-07-09 07:56:02 +0200

    Movimiento de ficheros

--- src/main/java/com/rod/rollenia/controller/ViewController.java
+++ src/main/java/com/rod/rollenia/controller/ViewController.java
@@ -1,4 +1,4 @@
-package com.rod.rollenia;
+package com.rod.rollenia.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/rod/rollenia/security/SecurityConfig.java
+++ src/main/java/com/rod/rollenia/security/SecurityConfig.java
@@ -1,6 +1,5 @@
-package com.rod.rollenia;
+package com.rod.rollenia.security;
 
-import com.rod.rollenia.security.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
